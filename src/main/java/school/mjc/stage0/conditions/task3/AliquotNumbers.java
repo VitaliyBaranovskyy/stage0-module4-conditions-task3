df3291d5e7502a9aef02fb2256d2760bf10245d7
VitaliyBaranovskyy <40470956+VitaliyBaranovskyy@users.noreply.github.com>
2023-02-24 12:35:03 +0200

    Update AliquotNumbers.java

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
+      
+         if (first %2 == 0 )
+    System.out.println("Aliquot");
+    
+    if (first %2 != 0  )
+    System.out.println("Not aliquot");
+    
+    }
+    
+    public static void main(String args[]) {
+        
+     AliquotNumbers main = new AliquotNumbers ();
+     main.isFirstAliquot(8,8);
+        
     }
 }

beeda4d6e106ce043dac04106742626ef20f9869
VitaliyBaranovskyy <40470956+VitaliyBaranovskyy@users.noreply.github.com>
2023-02-24 12:54:31 +0200

    Update IsEnglishSymbolDeterminer.java

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        
+         if (symbol >= 'A' & symbol <='z')
+   
+ System.out.println("English");
+
+  else
+  System.out.println("Non English");
+  
+  
+}
+public static void main(String[] args){
+    IsEnglishSymbolDeterminer main = new IsEnglishSymbolDeterminer();
+    
+    main.isEnglishSymbol('B');
+        
     }
 }
